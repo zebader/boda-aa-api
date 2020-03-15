5cd7245e42252f06e81b4d30c8d67ce1984b3ff1
Jesus Cebader <zebader@hotmail.com>
2020-03-15 14:11:53 +0100

    build

---
+++ public/precache-manifest.0315c47b007b1dbc5222f91c3cbdef1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f91ef2c9c0107f2088efda5b205183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f783c559b0bf1339d37",
+    "url": "/static/js/2.a610e531.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.a610e531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ff63aa48cc0cbaa9b75",
+    "url": "/static/js/main.d5f45083.chunk.js"
+  },
+  {
+    "revision": "bfab29e75fad7795d37b",
+    "url": "/static/js/runtime-main.3fb30b13.js"
+  }
+]);

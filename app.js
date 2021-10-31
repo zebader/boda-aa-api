b048126bf941ef56818cd58a5daf8a2722a5166d
jesus <jcebader@mediktor.com>
2021-10-31 19:03:03 +0100

    fix(headers): allowes methods

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 // Add headers
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin',  process.env.PUBLIC_DOMAIN);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
